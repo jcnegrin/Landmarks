0c2ed65946e8e4fefe9231a0e91e3ab86cac07fa
Juan Negrin <juan.negrin@electronicid.eu>
2022-09-22 20:57:01 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Juan Carlos Negrin on 22/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
